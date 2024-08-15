56373fc917ae6ea4d84ae8681735be9b15456155
Linkan Kumar Sahu <sahulinkan7@gmail.com>
2024-08-15 22:42:46 +0530

    file structure updated

--- template.py
+++ template.py
@@ -16,7 +16,7 @@ list_of_files=[
     f"{project_name}/constants/__init__.py",
     f"{project_name}/entity/__init__.py",
     f"{project_name}/entity/config_entity.py",
-    f"{project_name}/entity/artifact_entity.py"
+    f"{project_name}/entity/artifact_entity.py",
     f"{project_name}/exceptions/__init__.py",
     f"{project_name}/logger/__init__.py",
     f"{project_name}/pipeline/__init__.py",
